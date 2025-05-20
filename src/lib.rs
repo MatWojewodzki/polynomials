728365843161bb29ffb5fb958641823e505ad1bc
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2025-05-20 18:16:40 +0200

    Initial commit
    Initialized a library crate. Created Polynomial struct with core constructors and methods. Included unit tests for all the functionality.

---
+++ src/lib.rs
@@ -0,0 +1,137 @@
+use std::collections::HashMap;
+use std::fmt::{self, Display};
+
+pub struct Polynomial {
+    coefficients: HashMap<u32, f64>,
+}
+
+impl Polynomial {
+    pub fn zero() -> Polynomial {
+        Polynomial {
+            coefficients: HashMap::new(),
+        }
+    }
+
+    pub fn set_coefficient_at(&mut self, power: u32, coefficient: f64) {
+        if coefficient == 0.0 {
+            self.coefficients.remove(&power);
+            return;
+        }
+        self.coefficients.insert(power, coefficient);
+    }
+
+    pub fn get_coefficient_at(&self, power: u32) -> f64 {
+        self.coefficients.get(&power).copied().unwrap_or(0.0)
+    }
+
+    pub fn from_coefficients(coefficients: &Vec<f64>) -> Polynomial {
+        let mut poly = Polynomial::zero();
+        for (power, coefficient) in (0..coefficients.len()).rev().zip(coefficients.iter()) {
+            poly.set_coefficient_at(power as u32, *coefficient);
+        }
+        poly
+    }
+
+    pub fn is_zero(&self) -> bool {
+        self.coefficients.is_empty()
+    }
+
+    pub fn degree(&self) -> Option<u32> {
+        self.coefficients.keys().max().copied()
+    }
+}
+
+impl Display for Polynomial {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let None = self.degree() {
+            return write!(f, "0");
+        }
+
+        let mut repr = String::new();
+        let mut powers: Vec<_> = self.coefficients.keys().collect();
+        powers.sort_by(|a, b| b.cmp(a));
+
+        for power in powers {
+            let coefficient = self.coefficients.get(power).unwrap();
+
+            if *coefficient == 0.0 {
+                continue;
+            }
+
+            let sign = if *coefficient > 0.0 { "+" } else { "-" };
+
+            if *power != self.degree().unwrap() || sign == "-" {
+                repr.push_str(&format!(" {sign} "));
+            }
+
+            if *coefficient != 1.0 {
+                repr.push_str(&coefficient.abs().to_string());
+            }
+
+            if *power > 1 {
+                repr.push_str(&format!("x^{}", power));
+            } else if *power == 1 {
+                repr.push_str("x");
+            }
+        }
+        write!(f, "{}", repr)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_zero_works() {
+        let mut poly = Polynomial::zero();
+        assert!(poly.is_zero());
+        poly.set_coefficient_at(0, 3.0);
+        assert!(!poly.is_zero());
+    }
+
+    #[test]
+    fn from_coefficients_works_correctly() {
+        let poly = Polynomial::from_coefficients(&vec![2.0, 0.0, 2.0, -3.0]);
+        assert_eq!(poly.get_coefficient_at(3), 2.0);
+        assert_eq!(poly.get_coefficient_at(2), 0.0);
+        assert_eq!(poly.get_coefficient_at(1), 2.0);
+        assert_eq!(poly.get_coefficient_at(0), -3.0);
+    }
+
+    #[test]
+    fn degree_works() {
+        let mut poly = Polynomial::from_coefficients(&vec![-2.0]);
+        assert_eq!(poly.degree(), Some(0));
+
+        poly.set_coefficient_at(2, 3.0);
+        assert_eq!(poly.degree(), Some(2));
+
+        poly.set_coefficient_at(1, 2.0);
+        assert_eq!(poly.degree(), Some(2));
+
+        poly.set_coefficient_at(5, 0.0);
+        assert_eq!(poly.degree(), Some(2));
+
+        poly.set_coefficient_at(1234, 1.0);
+        assert_eq!(poly.degree(), Some(1234));
+    }
+
+    #[test]
+    fn degree_handles_zero_polynomial() {
+        let poly = Polynomial::zero();
+        assert_eq!(poly.degree(), None);
+    }
+
+    #[test]
+    fn to_string_handles_general_case() {
+        let poly = Polynomial::from_coefficients(&vec![1.0, 2.0, -3.0]);
+        assert_eq!(poly.to_string(), String::from("x^2 + 2x - 3"))
+    }
+
+    #[test]
+    fn to_string_handles_zero_polynomial() {
+        let poly = Polynomial::zero();
+        assert_eq!(poly.to_string(), "0");
+    }
+}
