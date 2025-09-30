a0482a0c045fcfa0d267e1a1071088c432931a09
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2025-09-30 20:03:03 +0200

    Provide documentation for the `coefficients` module and implement the `div_coefficient_at()` method

--- src/polynomial/coefficients.rs
+++ src/polynomial/coefficients.rs
@@ -1,6 +1,19 @@
+//! Module containing methods for manipulating the coefficients of a polynomial.
 use super::Polynomial;
 
 impl Polynomial {
+    /// Sets the coefficient in the term with the indeterminate raised to the given power.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use polynomials::Polynomial;
+    ///
+    /// let mut poly = Polynomial::zero();
+    /// poly.set_coefficient_at(0, 3.0);
+    /// poly.set_coefficient_at(3, -2.0);
+    /// assert_eq!(vec![-2.0, 0.0, 0.0, 3.0], poly.get_coefficients());
+    /// ```
     pub fn set_coefficient_at(&mut self, power: u32, coefficient: f64) {
         if coefficient == 0.0 {
             self.coefficients.remove(&power);
@@ -9,22 +22,106 @@ impl Polynomial {
         self.coefficients.insert(power, coefficient);
     }
 
+    /// Returns the coefficient from the term with the indeterminate raised to the given power.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use polynomials::Polynomial;
+    ///
+    /// let poly = Polynomial::from_coefficients(&vec![-1.0, 0.0, 3.0]);
+    /// assert_eq!(-1.0, poly.get_coefficient_at(2));
+    /// assert_eq!(0.0, poly.get_coefficient_at(1));
+    /// assert_eq!(3.0, poly.get_coefficient_at(0));
+    /// ```
     pub fn get_coefficient_at(&self, power: u32) -> f64 {
         self.coefficients.get(&power).copied().unwrap_or(0.0)
     }
 
+    /// Adds the specified value to the coefficient of the term with the indeterminate raised
+    /// to the given power.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use polynomials::Polynomial;
+    ///
+    /// let mut poly = Polynomial::from_coefficients(&vec![1.0, 3.0, -2.0]);
+    /// poly.add_coefficient_at(2, 3.0);
+    /// poly.add_coefficient_at(0, -1.0);
+    /// assert_eq!(vec![4.0, 3.0, -3.0], poly.get_coefficients());
+    /// ```
     pub fn add_coefficient_at(&mut self, power: u32, coefficient: f64) {
         self.set_coefficient_at(power, self.get_coefficient_at(power) + coefficient);
     }
 
+    /// Subtracts the specified value from the coefficient of the term with the indeterminate
+    /// raised to the given power.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use polynomials::Polynomial;
+    ///
+    /// let mut poly = Polynomial::from_coefficients(&vec![1.0, 3.0, -2.0]);
+    /// poly.sub_coefficient_at(2, 3.0);
+    /// poly.sub_coefficient_at(0, -1.0);
+    /// assert_eq!(vec![-2.0, 3.0, -1.0], poly.get_coefficients());
+    /// ```
     pub fn sub_coefficient_at(&mut self, power: u32, coefficient: f64) {
         self.set_coefficient_at(power, self.get_coefficient_at(power) - coefficient);
     }
 
+    /// Multiplies the coefficient of the term with the indeterminate raised to the given power
+    /// by the specified value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use polynomials::Polynomial;
+    ///
+    /// let mut poly = Polynomial::from_coefficients(&vec![1.0, 3.0, -2.0]);
+    /// poly.mul_coefficient_at(2, 3.0);
+    /// poly.mul_coefficient_at(1, -2.0);
+    /// poly.mul_coefficient_at(0, 0.0);
+    /// assert_eq!(vec![3.0, -6.0, 0.0], poly.get_coefficients());
+    /// ```
     pub fn mul_coefficient_at(&mut self, power: u32, coefficient: f64) {
         self.set_coefficient_at(power, self.get_coefficient_at(power) * coefficient);
     }
 
+    /// Divides the coefficient of the term with the indeterminate raised to the given power
+    /// by the specified value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use polynomials::Polynomial;
+    ///
+    /// let mut poly = Polynomial::from_coefficients(&vec![1.0, 3.0, -2.0]);
+    /// poly.div_coefficient_at(2, 2.0);
+    /// poly.div_coefficient_at(0, -2.0);
+    /// assert_eq!(vec![0.5, 3.0, 1.0], poly.get_coefficients());
+    /// ```
+    pub fn div_coefficient_at(&mut self, power: u32, coefficient: f64) {
+        self.set_coefficient_at(power, self.get_coefficient_at(power) / coefficient);
+    }
+
+    /// Creates a `Polynomial` instance from a vector of coefficients.
+    ///
+    /// The coefficients must specify subsequent terms sorted by their degree in descending order,
+    /// with the last coefficient in the vector specifying the term of degree zero.
+    ///
+    /// # Examples
+    ///
+    /// Represent quadratic polynomial `x^2 + x -2` as a `Polynomial` instance:
+    /// ```
+    /// use polynomials::Polynomial;
+    ///
+    /// let coefficients = vec![1.0, 1.0, -2.0];
+    /// let poly = Polynomial::from_coefficients(&coefficients);
+    /// assert_eq!(coefficients, poly.get_coefficients());
+    /// ```
     pub fn from_coefficients(coefficients: &Vec<f64>) -> Polynomial {
         let mut poly = Polynomial::zero();
         for (power, coefficient) in (0..coefficients.len()).rev().zip(coefficients.iter()) {
